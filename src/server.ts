fcdbc1608771cb19472a2db1bdc97a10de2ce6df
TolerTomas <t.toler@alumno.etec.um.edu.ar>
2024-09-05 19:31:40 -0300

    new cors configuration

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,14 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 
+// const allowedOrigins = ['http://localhost:8080', 'http://127.0.0.1:3030']
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: '*'
-}))
+    origin: '*',
+    credentials: true
+  }));
 
 app.use('/api/users', userAuthRouter)
 app.use('/api/servers', serverAuthRouter)

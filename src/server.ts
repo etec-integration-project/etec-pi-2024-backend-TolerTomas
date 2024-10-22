bd33bfe9b2746044a4498a2abe0bddf7dbed7b34
TolerTomas <t.toler@alumno.etec.um.edu.ar>
2024-10-22 18:22:41 -0300

    22-10-2024

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import { userAuthRouter } from './routes/user-auth.routes'
-import { serverAuthRouter } from './routes/server-auth.routes'
-import cookieParser from 'cookie-parser'
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { userAuthRouter } from "./routes/user-auth.routes";
+import { serverAuthRouter } from "./routes/server-auth.routes";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-// const allowedOrigins = ['http://localhost:8080', 'http://127.0.0.1:3030']
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-    origin: '*',
-    credentials: true
-  }));
+app.get("/health", (_req, res) => {
+	res.send("I am healthy");
+});
 
-app.use('/api/users', userAuthRouter)
-app.use('/api/servers', serverAuthRouter)
+app.use("/api/users", userAuthRouter);
+app.use("/api/servers", serverAuthRouter);
 
-export default app
+export default app;
